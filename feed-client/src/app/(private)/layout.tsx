8eb4df4afcc04bc1d76877ac49ef133db6181a84
dangphu2412 <dangphu241299@gmail.com>
2025-05-04 11:37:49 +0700

    feat: implement imple togs page

--- feed-client/src/app/(private)/layout.tsx
+++ feed-client/src/app/(private)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import {Footer} from "@/features/shared/layout/Footer";
 import {AuthProvider} from "@/features/shared/layout/AuthProvider";
 
